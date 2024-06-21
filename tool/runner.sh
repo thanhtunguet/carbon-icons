ad8bf3c182794b4cac2f128f6e5b137599c7396b
thanhtunguet <thanhtung.uet@gmail.com>
2024-06-21 17:14:18 +0700

    Update docs

--- tool/runner.sh
+++ tool/runner.sh
@@ -6,6 +6,11 @@ dart font_generator.dart
 
 echo "formatting generated dart file"
 dart format generated/carbon_fonts.dart
+
 cp generated/carbon_fonts.dart ../lib/src/fonts/carbon_fonts.dart
+cp -r generated/* ../docs
+cp generated/CarbonFonts.ttf ../assets
+
+mv ../docs/CarbonFonts.html ../docs/index.html
 
 exec $SHELL;
